3da752d82a5b124e07bc43b3b5021ab0dd18c141
[xZenshin] <alsk@itu.dk>
2023-11-27 16:27:54 +0100

    fixed few test and system test

--- RedScare/UnitTests/FewTests/FewTests.cs
+++ RedScare/UnitTests/FewTests/FewTests.cs
@@ -10,7 +10,7 @@ public class FewTests
 	[Fact]
 	public void G_ex_finds_path_size_0()
 	{
-        var expected = 0;
+        var expected = 9;
         Graph graph = GraphParser.ParseGraph("ski-level20-2.txt", true);
         var actual = Few.FewestNumberOfRed(graph);
         Assert.Equal(expected, actual);
